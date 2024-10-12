57bee10a1dcf4287c813ebe0df91ad39f8e0639c
GonZiin <goncalogomespessoal@outlook.pt>
2024-10-12 16:05:01 +0100

    exercicio 1_b

---
+++ 1_b.c
@@ -0,0 +1,23 @@
+/*1. Implemente um programa que permita calcular a área de um quadrado. 
+(Área=Lado*Lado), visualizando o resultado obtido, com: 
+b) o valor do lado deverá ser pedido ao utilizador.*/
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int area, lado;
+
+    printf("Introduza o comprimento do lado do quadrado: ");
+    scanf("%d", &lado);
+
+    area = lado * lado;
+
+    printf("A área do quadrado será: %d cm²\n", area);
+
+    return 0;
+}
+
