b4d33acf017d284e6a9cdb73bac2465a357c1f49
GonZiin <goncalogomespessoal@outlook.pt>
2024-10-13 14:23:57 +0100

    Correcao do 14

--- Programação Procedimental/B. Estruturas de decisão/14.c
+++ Programação Procedimental/B. Estruturas de decisão/14.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main(void)
 {
+    setlocale(LC_ALL, "Portuguese");
+
     float nota;
 
     printf("Introduza a sua nota: ");
     scanf(" %f", &nota);
 
-    if(nota >= 0 && nota < 5){
-        printf("pessimo\n");
-    }else if(nota >= 5 && nota < 8){
+    if (nota >= 0 && nota < 5) {
+        printf("péssimo\n");
+    } else if (nota >= 5 && nota < 8) {
         printf("mau\n");
-    }else if(nota >= 8 && nota < 10){
+    } else if (nota >= 8 && nota < 10) {
         printf("insuficiente\n");
-    }else if(nota >= 10 && nota < 12){
+    } else if (nota >= 10 && nota < 12) {
         printf("suficiente\n");
-    }else if(nota >= 12 && nota <16){
+    } else if (nota >= 12 && nota < 16) {
         printf("bom\n");
-    }else if(nota >= 16 && nota < 18){
+    } else if (nota >= 16 && nota < 18) {
         printf("muito bom\n");
-    }else if(nota >= 18 && nota < 20){
-        printf("excelente");
-    }else
-        printf("Nota invalida.\n");
-    
+    } else if (nota >= 18 && nota < 20) {
+        printf("excelente\n");
+    } else {
+        printf("Nota inválida.\n");
+    }
+
     return 0;
 }
