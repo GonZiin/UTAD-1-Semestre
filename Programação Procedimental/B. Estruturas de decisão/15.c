c0a7a4cd4718a35b685ddd88d5261c4d55ce8020
GonZiin <goncalogomespessoal@outlook.pt>
2024-10-13 14:26:54 +0100

    Exercicio 15

--- Programação Procedimental/B. Estruturas de decisão/15.c
+++ Programação Procedimental/B. Estruturas de decisão/15.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
+#include <locale.h>
 
-int 
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int numero;
+
+    printf("Introduza um número: ");
+    scanf("%d", &numero);
+
+    if (numero % 2 != 0)
+        printf("O número %d é ímpar\n", numero);
+    else
+        printf("O número %d é par\n", numero);
+
+    return 0;   
+}
