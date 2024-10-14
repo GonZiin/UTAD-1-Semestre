f9889f906244248bda2d35447df90502617457cf
GonZiin <goncalogomespessoal@outlook.pt>
2024-10-14 13:14:13 +0100

    Exercicio 32

--- Programação Procedimental/C. Estruturas de repetição/32.c
+++ Programação Procedimental/C. Estruturas de repetição/32.c
@@ -1,11 +1,26 @@
+/*. Dada uma série de 20 valores reais, implemente um programa que calcule e 
+escreva a média aritmética destes valores. Entretanto se a média obtida for maior 
+que 20 deverá ser atribuído o valor 20 para a média*/
+
 #include <stdio.h>
+#include <locale.h>
 
-int main(void)
-{
-    int nota;
+int main(void) {
+    setlocale(LC_ALL, "Portuguese");
 
-    for(int i = 0; i < 20; i++);
-        printf("Introduza a nota %d: ", i);
+    float nota, media = 0.0;
+    int i;
 
+    for(i = 0; i < 20; i++) {
+        printf("Introduza a nota %d: ", i + 1);
+        scanf("%f", &nota);
+        media += nota;
+    }
+
+    media /= 20;
+    if(media > 20)
+        media = 20;
+
+    printf("Média: %.2f\n", media);
     return 0;
 }
